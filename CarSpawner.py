0196bf86c4973b67e8b0432156d95ca6dd15fe81
lmtuan <lmtuan98@gmail.com>
2019-12-19 04:40:06 -0500

    Car spawner

---
+++ CarSpawner.py
@@ -0,0 +1,20 @@
+import random
+from Car import Car
+
+class CarSpawner:
+    def __init__(self, spawn_prob, destinations, dest_weights=None):
+        self.spawn_prob = spawn_prob
+        self.destinations = destinations
+        self.dest_weights = dest_weights
+
+    def set_out_lane(self, direction, lane):
+        self.out_lane = lane
+
+    def update(self):
+        # spawn car with probability self.spawn_prob
+        r = random.random()
+        if r > self.spawn_prob:
+            return
+        destination = random.choices(destinations, weights=self.dest_weights)
+        car = Car(destination)
+        self.out_lane.enter(car)
